071c529f4dd7f32606ee1130befe564cfb6b13ad
JefG67 <jefguilpain@gmail.com>
2024-02-21 11:40:38 +0100

    ajouter la fonction remelanger en cas erreur

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+//pour x nombre de boite rentrer alors add moi x nombre de boite 
 
+// function nbrBox(){
+//     //cree une variable pour stocker le nombre de box
+//     let joueur = prompt("Nombre de Box au départ pour commencer a jouer");
+//     //on verifie que la valeur stockée par l'utilisateur n'est pas nul ( si il a bien rentré une valeur) pour poouvoir executer le bloc
+//     if(joueur !=null){
 
+//         const board = document.querySelector("#board")
+//         const box = document.createElement("div")
 
+//         for(let i = 0; i <= joueur; i++){
+//             const newbox = box.cloneNode();
+//             newbox.innerText = i;
+//             board.appendChild(newbox);
+
+            
+
+//         }
+            
+//     }
+
+// }
+// nbrBox();
 
 
 
@@ -18,46 +39,67 @@ function shuffleChildren(parent){
     
 }
 
+function showReaction(type,clickBox){
+    clickBox.classList.add(type)
+    if(type !=="sucess"){
+        setTimeout(function(){
+            clickBox.classList.remove(type)
+        }, 800)
+    }
+}
 
 
 const box = document.createElement("div")
 box.classList.add("box")
-
-
+// const butt = document.querySelector("")
 
 
 const board = document.querySelector("#board")
 
-let nb = 1
 
-// On cree une boucle pour rajouter 10 box
-for (let i = 1; i <= 10; i++) {
+
+let nb = 1
+let joueur = prompt("Nombre de Box au départ pour commencer a jouer");
+if(joueur !=null){
+
+
+for (let i = 1; i <= joueur; i++) {
     const newbox = box.cloneNode();
     newbox.innerText = i;
-    board.appendChild(newbox);
+    board.appendChild(newbox); //ajoute une box au conteneur (board)
 
     newbox.addEventListener("click", function(){
         if(i == nb){
-        // console.log("Boite n° " + i + ", click !")
-        newbox.classList.add("box-valid")
-
+            newbox.classList.add("box-valid")
             if(nb == board.children.length){
-                alert("VICTOIRE !")
+                board.querySelectorAll(".box").forEach(function(box){
+                    showReaction("succes",box)
+                })
             }
             nb++
+
+                
         }
         else if (i > nb){
-            alert ("Erreur, recommencez !")
+            showReaction ("error", newbox)
             nb = 1
             board.querySelectorAll(".box-valid").forEach(function(valideBox){valideBox.classList.remove("box-valid")
         })
         }
 
         else{
-            alert("Case déjà cliquée !")
+            showReaction("notice",newbox)
         }
     })
 
 }
+}
 
 shuffleChildren(board)
+
+
+
+
+
+
+
